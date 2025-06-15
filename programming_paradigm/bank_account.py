f64e89d8fe01317c3e26ea0f4f61d1a27234f1da
iaustinewise <iaustinewise@gmail.com>
2025-06-15 17:18:38 +0100

    Fix display_balance to output plain number

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,4 +1,3 @@
-
 import json
 
 class BankAccount:
@@ -26,7 +25,7 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self._account_balance:.2f}")
+        print(f"{self._account_balance:.2f}")
 
     def save_balance(self):
         with open(self._filename, 'w') as f:
